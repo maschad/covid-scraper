de366e6ed39ec4b7b91f32d997ece4739a2ce68a
maschad <chad.nehemiah94@gmail.com>
2020-03-26 19:55:08 -0500

    completed docker and added cors

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 /** @format */
 
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 const scrapeData = require("./scraper").scrapeData;
 
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || "0.0.0.0";
+
+app.use(cors());
 
 app.get("/", async (req, res) => {
 	const results = await scrapeData();
